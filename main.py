800f7551d9fec44e55e67d2a65e919bba592186a
Cuong Tran <tranhuucuong91@gmail.com>
2015-12-12 11:59:32 +0700

    Refactoring code, fix typo

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
+#!/usr/bin/env python3
+
+import logging
+import sys
+
 from nbc_watcher import Watcher
 
+
 def main():
+    logging.basicConfig(stream=sys.stderr, level=logging.INFO)
 
     def total_condition(response):
         return response['hits']['total'] > 0
 
-    watcher = Watcher("configs.json")
+    watcher = Watcher("config.json")
 
-    watcher.add_send_gmail_action()
+    # watcher.add_send_gmail_action()
+    watcher.add_send_log_action()
     watcher.add_condition(total_condition)
     watcher.run()
 
--- nbc_watcher.py
+++ nbc_watcher.py
@@ -1,105 +1,103 @@
-#! /usr/bin/env python3
-import threading
-import logging
 import json
-import schedule 
+import logging
 import smtplib
-from elasticsearch import Elasticsearch
-from elasticsearch_dsl import Search
+import threading
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
+import schedule
+from elasticsearch import Elasticsearch
+
 
 class Watcher:
-    """Class watching the logs
-    
+    """Class watching the log
+
     Class will base conditions to implement actions
     """
     actions = []
     conditions = []
-    def __init__(self,configs_file):
+
+    def __init__(self, config_file):
         """Init Object base on file config
-    
-        @param string configs_file, name of file json config
+
+        @param string config_files, name of file json config
         """
         self.logger = logging.getLogger(__name__)
-        self.logger.setLevel(logging.INFO)
         self.logger.info("Loading config file...")
-        with open(configs_file) as configs_file:    
-            self.configs = json.load(configs_file)
+        with open(config_file) as config_file:
+            self.config = json.load(config_file)
         self.logger.info("Create searcher...")
-        self.searcher = Elasticsearch(self.configs["eslaticsearch"])
+        self.searcher = Elasticsearch(self.config["eslaticsearch"])
 
-    def add_action(self,action):
+    def add_action(self, action):
         """Add action for watcher
 
         @param function action, is a function have only one param (response of Elasticsearch.search)
         """
-        self.logger.info("add action "+action.__name__)
+        self.logger.info("add action " + action.__name__)
         self.actions.append(action)
 
-    def add_condition(self,condition):
+    def add_condition(self, condition):
         """Add condition for watcher
 
         @param function condition, is a function have only one param (response of Elasticsearch.search) and return boolean value
         """
-        self.logger.info("add condition "+condition.__name__)
+        self.logger.info("add condition " + condition.__name__)
         self.conditions.append(condition)
 
     def add_send_gmail_action(self):
-        """Action buil-in class Watcher, send gmail action"""
+        """Action built-in class Watcher, send gmail action"""
+
         def send_gmail_action(response):
             msg = MIMEMultipart()
             msg['Subject'] = 'NBC Watcher Notification'
-            msg['From'] = self.configs["action"]["gmail"]["from"]["user"]
-            msg['To'] = self.configs["action"]["gmail"]["to"]["user"]
-            text_msg = MIMEText(self.configs["action"]["gmail"]["msg"].format(response=response), 'plain')
+            msg['From'] = self.config["actions"]["gmail"]["from"]["user"]
+            msg['To'] = self.config["actions"]["gmail"]["to"]["user"]
+            text_msg = MIMEText(self.config["actions"]["gmail"]["msg"].format(response=response), 'plain')
             msg.attach(text_msg)
 
             gmail = smtplib.SMTP('smtp.gmail.com:587')
             gmail.ehlo()
             gmail.starttls()
-            gmail.login(self.configs["action"]["gmail"]["from"]["user"], self.configs["action"]["gmail"]["from"]["pass"])
-            gmail.sendmail(self.configs["action"]["gmail"]["from"]["user"], self.configs["action"]["gmail"]["to"]["user"], msg)
+            gmail.login(self.config["actions"]["gmail"]["from"]["user"],
+                        self.config["actions"]["gmail"]["from"]["pass"])
+            gmail.sendmail(self.config["actions"]["gmail"]["from"]["user"],
+                           self.config["actions"]["gmail"]["to"]["user"], msg)
             gmail.quit()
+
         self.add_action(send_gmail_action)
 
-    def send_to_logs_action(self):
-        def to_logs_action(response):
-            self.logger.info(self.configs["action"]["logs"]["format"].format(response=response))
-        self.add_action(to_logs_action)
+    def add_send_log_action(self):
+        def send_log_action(response):
+            self.logger.info(self.config["actions"]["log"]["format"].format(response=response))
+
+        self.add_action(send_log_action)
 
     def watching(self):
-        """Query logs in eslaticsearch and compare condition then implement action"""
+        """Query log in eslaticsearch and compare condition then implement action"""
         self.logger.info("Searching in eslaticsearch...")
-        response = self.searcher.search(index=self.configs["index"], body=self.configs["search"])
-        self.logger.info("Totol hits: "+ str(response['hits']['total']))
+        response = self.searcher.search(index=self.config["index"], body=self.config["search"])
+        self.logger.debug("Total hits: " + str(response['hits']['total']))
         self.logger.debug(response)
+
         result = True
-        self.logger.info("Checking conditions...")
+        self.logger.debug("Checking conditions...")
         for condition in self.conditions:
             result = result and condition(response)
 
-        self.logger.info("Result = "+str(result))
-        if(result == True):
+        self.logger.debug("Result = " + str(result))
+        if (result == True):
             for action in self.actions:
                 try:
-                    self.logger.info("Implement action "+action.__name__)
-                    action_thread = threading.Thread(target=action,args=(response,), daemon = True)
+                    self.logger.debug("Implement action " + action.__name__)
+                    action_thread = threading.Thread(target=action, args=(response,), daemon=True)
                     action_thread.start()
-                except Exception:
-                    pass
+                except Exception as e:
+                    self.logger.error(e)
 
     def run(self):
         """Run watcher"""
         self.logger.info("Running watcher ...")
-        schedule.every(self.configs["interval"]).seconds.do(self.watching)
+        schedule.every(self.config["interval"]).seconds.do(self.watching)
         while True:
             schedule.run_pending()
-
-
-
-
-
-
-
